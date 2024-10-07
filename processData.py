fa633ac1baa6a66e304e8694e32392ae7be6f6e5
shashankraj1987 <shashank1_theone@yahoo.co.in>
2024-10-07 13:22:09 +0000

    Will send email for last 5 days

--- processData.py
+++ processData.py
@@ -1,11 +1,14 @@
 import getSheetData
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
-def get_curr_date():
+def get_curr_date(days: int = None):
     now = datetime.now()
-    date_str = now.strftime("%Y-%m-%d")
+    if days is not None:
+        date_str = [day for day in datetime.now() - timedelta(days=5)]
+    else:
+        date_str = [now.strftime("%Y-%m-%d")]
     return date_str
 
 
@@ -13,14 +16,11 @@ def get_update():
     values = getSheetData.read_values()
     df = pd.DataFrame(values)
     df.columns = ["Date", "Name", "Update", "Status"]
-    # print(df)
     ref_date = get_curr_date()
-    fil_df = df[df["Date"] == ref_date].reset_index(drop=True)
+    fil_df = df[df["Date"].isin(ref_date)].reset_index(drop=True)
     # fil_df = df
     msg = []
     for x in fil_df.index:
-        # print(x)
-        # print(fil_df.iloc[x])
         msg.append(fil_df.iloc[x])
     return msg
 
